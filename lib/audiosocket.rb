b4e13c713e86e2620c2ad56c0c5ea11c435dfc92
John Barnette <jbarnette@gmail.com>
2010-05-11 20:35:03 -0700

    Basic config and dep cleanup.

--- lib/audiosocket.rb
+++ lib/audiosocket.rb
@@ -1,6 +1,22 @@
+require "configlet"
+require "rest_client"
+require "uri"
+
 module Audiosocket
+  extend Configlet
 
   # Duh.
 
   VERSION = "1.0.0"
+
+  config :audiosocket do
+    default :token => "no-token-provided"
+    default :url   => "http://audiosocket.com/api/v3"
+
+    munge(:url) { |url| URI.parse url  }
+  end
+
+  RestClient.add_before_execution_proc do |req, params|
+    p :req => req, :params => params
+  end
 end
